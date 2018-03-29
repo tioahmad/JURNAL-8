0a9215d3f83cc9fb7e1612283d149b2f56a4dd78
tioahmad_6706160068 <37782743+tioahmad@users.noreply.github.com>
2018-03-29 08:24:58 +0700

    UPLOAD FIBONACCI

---
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci :");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 1;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+    
+}
